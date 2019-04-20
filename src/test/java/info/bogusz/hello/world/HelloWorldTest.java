5253470ad6d576e4ef31a74148ed1d9d5469be50
Piotr Bogusz <piotr.bogusz@altkom.pl>
2019-04-20 22:49:23 +0200

    Initial commit
    Change-Id: I47738f33273be553440e1749405e44580e6ea463

---
+++ src/test/java/info/bogusz/hello/world/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package info.bogusz.hello.world;
+
+import org.junit.jupiter.api.Test;
+
+import static java.lang.System.out;
+
+class HelloWorldTest {
+
+    @Test
+    void helloWorld() {
+        out.println("hello world");
+    }
+}
